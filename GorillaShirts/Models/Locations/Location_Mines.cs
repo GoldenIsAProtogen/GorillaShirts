cc91c306c941cc857b4754437a93b7a092935c56
Golden <goldenisaprotogen@tutamail.com>
2025-11-03 23:32:25 -0500

    Add files via upload

---
+++ GorillaShirts/Models/Locations/Location_Mines.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+namespace GorillaShirts.Models.Locations
+{
+    internal class Location_Mines : Location_Base
+    {
+        public override GTZone[] Zones => [GTZone.mines];
+        public override Vector3 Position => new(-45.7829f, -7.323f, -76.7468f);
+        public override Vector3 EulerAngles => Vector3.up * 53.7666f;
+    }
+}
